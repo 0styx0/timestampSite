4a0fa2c4947a67b1cd284383578fba1b00211b47
DovidM <landyx47@gmail.com>
2017-01-10 23:31:37 -0500

    Completed challenge at https://www.freecodecamp.com/challenges/timestamp-microservice

---
+++ timeStamp.js
@@ -0,0 +1,28 @@
+var express = require('express');
+
+
+    var app = express();
+    
+    app.use(function(req, res) {
+        var url = decodeURI(req.url.slice(1));
+        
+    var dateUrl, dateString, unixTime;
+
+    if (typeof +url == "String") {
+        dateUrl = new Date(url);
+
+        dateString = dateUrl.toDateString();
+    }
+    else {
+        dateUrl = new Date().setSeconds(url);
+
+        dateString = new Date(dateUrl).toDateString();
+    }
+
+        unixTime = Date.parse(dateString);
+
+        res.end(JSON.stringify({"natural": dateString, "unix": unixTime}));
+    });
+
+    app.listen(8080);
+
